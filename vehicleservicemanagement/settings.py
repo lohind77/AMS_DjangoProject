f03fb11502a450c165c3915bdd89ca42c635db31
Lohind Chintada <lohindchintada77@gmail.com>
2023-06-27 16:38:12 +0530

    done migrations

--- vehicleservicemanagement/settings.py
+++ vehicleservicemanagement/settings.py
@@ -89,7 +89,7 @@ DATABASES={
       'ENGINE':'django.db.backends.postgresql_psycopg2',
       'NAME':'postgres',
       'USER':'postgres',
-      'PASSWORD':'Akhil2002',
+      'PASSWORD':'Lohind@77',
       'HOST':'localhost',
       'PORT':'5432',
    }
